d3e2ab1f47304dc6719bd476da2d9e07fca4c419
h19960626 <h19960626@gmail.com>
2020-12-04 14:16:03 +0900

    북마크 기능 버그 수정 완료, select box 버그 수정 완료

--- src/Pages/CurrencyDetail/CurrencyDetail.js
+++ src/Pages/CurrencyDetail/CurrencyDetail.js
@@ -2,25 +2,26 @@ import React, { useState, useEffect } from "react";
 import "./CurrencyDetail.scss";
 import axios from "axios";
 
-export default function CurrencyDetail() {
+export default function CurrencyDetail({ match }) {
   const [currencyData, setCurrencyData] = useState([]);
-  console.log(currencyData);
   const getApiData = async () => {
     try {
-      const basicUrl = `https://api.coingecko.com/api/v3/coins/bitcoin`;
+      const basicUrl = `https://api.coingecko.com/api/v3/coins/${match.params.id}`;
       const response = await axios.get(basicUrl);
-      const apiData = response.data.map((el) => ({
-        id: el.id,
-        name: el.name,
-        symbol: el.symbol.toUpperCase(),
-        image: el.image.small,
-        rank: el.market_cap_rank,
-        website: el.links.homepage[0],
-        price: el.market_data.current_price,
-        market_cap: el.market_cap,
-        total_volume: el.total_volume,
-        description: el.description,
-      }));
+      // console.log(response.data)
+      const apiData = {
+        id: response.data.id,
+        name: response.data.name,
+        symbol: response.data.symbol.toUpperCase(),
+        image: response.data.image.small,
+        rank: response.data.market_cap_rank,
+        website: response.data.links.homepage[0],
+        price: response.data.market_data.current_price,
+        market_cap: response.data.market_cap,
+        total_volume: response.data.total_volume,
+        description: response.data.description,
+      };
+      console.log(apiData)
       setCurrencyData(apiData);
     } catch (error) {
       console.log(error);
@@ -29,5 +30,6 @@ export default function CurrencyDetail() {
   useEffect(() => {
     getApiData();
   }, []);
+  console.log(currencyData)
   return <div>hello World</div>;
 }
--- src/Pages/CurrencyList/Component/SelectBox/SelectBox.js
+++ src/Pages/CurrencyList/Component/SelectBox/SelectBox.js
@@ -1,18 +1,29 @@
-import React, { useState, useEffect } from "react";
-import { Link, useHistory } from "react-router-dom";
+import React from "react";
+import { useHistory } from "react-router-dom";
 import "./SelectBox.scss";
 
-export default function SelectBox() {
+export default function SelectBox({
+  setVsCurrency,
+  setPerPage,
+  perPage,
+  vsCurrency,
+}) {
   const CURRENT_PAGE = ["전체보기", "북마크보기"];
   const VS_CURRENCY = ["KRW 보기", "USD 보기"];
   const PER_PAGE = ["50개 보기", "30개 보기", "10개 보기"];
-
   const history = useHistory();
 
   const handlePage = (e) => {
     e.target.value === "북마크보기"
-      ? history.push("bookmark")
-      : history.push("currencylist");
+      ? history.push("/bookmark")
+      : history.push("/currencylist");
+  };
+  const handleVsCurrencyApi = (e) => {
+    setVsCurrency(e.target.value.slice(0, 3).toLowerCase());
+  };
+
+  const handlePerPageApi = (e) => {
+    setPerPage(e.target.value.slice(0, 2));
   };
 
   return (
@@ -22,14 +33,24 @@ export default function SelectBox() {
           <option key={idx}>{el}</option>
         ))}
       </select>
-      <select className="selectType">
+      <select className="selectType" onClick={(e) => handleVsCurrencyApi(e)}>
         {VS_CURRENCY.map((el, idx) => (
-          <option key={idx}>{el}</option>
+          <option
+            key={idx}
+            selected={vsCurrency === el.slice(0, 3).toLowerCase() ? "selected" : ""}
+          >
+            {el}
+          </option>
         ))}
       </select>
-      <select className="selectType">
+      <select className="selectType" onClick={(e) => handlePerPageApi(e)}>
         {PER_PAGE.map((el, idx) => (
-          <option key={idx}>{el}</option>
+          <option
+            key={idx}
+            selected={perPage === el.slice(0, 2) ? "selected" : ""}
+          >
+            {el}
+          </option>
         ))}
       </select>
     </nav>
